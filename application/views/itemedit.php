6099f0b571f0740bac37bd56803ad69d7d6b794c
ZhiyuanEric <linzhiyuaneric@yahoo.com>
2018-03-03 14:51:23 -0800

    job11 done
    Signed-off-by: ZhiyuanEric <linzhiyuaneric@yahoo.com>

--- application/views/itemedit.php
+++ application/views/itemedit.php
@@ -1,9 +1,13 @@
 <h1>Task # {id}</h1>
 <form role="form" action="/mtce/submit" method="post">
-    {ftask}
-    {fpriority}
-    {zsubmit}
+    {ftask}<br>
+    {fpriority}<br>
+    {size}<br>
+    {group}<br>
+    {status}<br>
+    {zsubmit}<br>
 </form>
+<br>
     {error}
 <a href="/mtce/cancel"><input type="button" value="Cancel the current edit"/></a>
 <a href="/mtce/delete"><input type="button" value="Delete this todo item"/></a>
