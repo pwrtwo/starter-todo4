01a639f400acea281af417e3911bc067dab79113
ZhiyuanEric <linzhiyuaneric@yahoo.com>
2018-03-26 14:10:28 -0700

    Loading function imported but not working yet

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -1,9 +1,10 @@
 <?php
 
-class Tasks extends CSV_Model {
-
+class Tasks extends XML_Model {
+    private $CI; // use this to reference the CI instance
     public function __construct() {
-        parent::__construct(APPPATH . '../data/tasks.csv', 'id');
+        parent::__construct(APPPATH . '../data/tasks.xml', 'id');
+        $this->CI = &get_instance(); // retrieve the CI instance
     }
 
     function getCategorizedTasks() {
@@ -14,7 +15,7 @@ class Tasks extends CSV_Model {
         }
         // substitute the category name, for sorting
         foreach ($undone as $task)
-            $task->group = $this->app->group($task->group);
+            $task->group = $this->CI->app->group($task->group); // use CI to get at the app model
         // order them by category
         usort($undone, "orderByCategory");
         // convert the array of task objects into an array of associative objects       
@@ -33,7 +34,38 @@ class Tasks extends CSV_Model {
         );
         return $config;
     }
+    
+    public function load() {
+        if (($tasks = simplexml_load_file($this->_origin)) !== FALSE)
+	{		foreach ($tasks as $task) {
+				$record = new stdClass();
+				$record->id = (int) $task->id;
+				$record->task = (string) $task->desc;
+				$record->priority = (int) $task->priority;
+				$record->size = (int) $task->size;
+				$record->group = (int) $task->group;
+				$record->deadline = (string) $task->deadline;
+				$record->status = (int) $task->status;
+				$record->flag = (int) $task->flag;
+				$this->_data[$record->id] = $record;
+			}
+		}
 
+		// rebuild the keys table
+		$this->reindex();
+        
+    }
+
+    public function store() {
+        if (($handle = fopen($this->_origin, "w")) !== FALSE)
+        {
+        fputcsv($handle, $this->_fields);
+	foreach ($this->_data as $key => $record)
+            fputcsv($handle, array_values((array) $record));
+	fclose($handle);
+        }
+    }
+    
 }
 
 function orderByCategory($a, $b) {
